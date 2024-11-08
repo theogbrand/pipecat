d22ced5dd4b6961aec4cefee8066946be4cae83e
James Hush <james@daily.co>
2024-11-08 16:24:57 +0800

    test: batch dialout demo

---
+++ examples/foundational/batch_dialin_demo.py
@@ -0,0 +1,57 @@
+import asyncio
+import csv
+import os
+from datetime import datetime
+
+import aiohttp
+
+from pipecat.transports.services.helpers.daily_rest import (
+    DailyRESTHelper,
+    DailyRoomParams,
+    DailyRoomProperties,
+)
+
+
+async def run_bot(id: int, csv_writer):
+    async with aiohttp.ClientSession() as aiohttp_session:
+        print(f"\nStarting bot number: {id}\n")
+        rest = DailyRESTHelper(
+            daily_api_key=os.getenv("DAILY_API_KEY", ""),
+            daily_api_url=os.getenv("DAILY_API_URL", "https://api.daily.co/v1"),
+            aiohttp_session=aiohttp_session,
+        )
+        # Create daily.co room with dialin and dialout enabled
+        room_params = DailyRoomParams(properties=DailyRoomProperties(enable_dialout=True))
+
+        # Create the room with the specified parameters
+        room = await rest.create_room(room_params)
+        token = await rest.get_token(room.url, 60 * 60, True)
+        print(f"{id}: Room Token: {token}")
+
+        # Check the room properties three times waiting 1 second between each check
+        for i in range(3):
+            room_info = await rest.get_room_from_url(room.url)
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+            csv_writer.writerow([id, room_info.config.enable_dialout, current_time])
+            await asyncio.sleep(1 * i)
+
+
+async def main():
+    # Open the CSV file in append mode
+    with open("output.csv", mode="w", newline="") as file:
+        csv_writer = csv.writer(file)
+        # Write the header row
+        csv_writer.writerow(["bot_id", "enable_dialout", "timestamp"])
+
+        bots = [run_bot(i, csv_writer) for i in range(10)]
+        await asyncio.gather(*bots)
+        bots = [run_bot(i, csv_writer) for i in range(11, 21)]
+        await asyncio.gather(*bots)
+        bots = [run_bot(i, csv_writer) for i in range(21, 31)]
+        await asyncio.gather(*bots)
+        bots = [run_bot(i, csv_writer) for i in range(31, 41)]
+        await asyncio.gather(*bots)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
