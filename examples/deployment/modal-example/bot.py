fac1f12717b003ab60662a91355a6c7e6fefa8dd
ob1 <ongjjbrandon@gmail.com>
2024-12-11 17:38:13 +0800

    added SEALION class

--- examples/deployment/modal-example/bot.py
+++ examples/deployment/modal-example/bot.py
@@ -21,6 +21,35 @@ async def main(room_url: str, token: str):
     from pipecat.services.cartesia import CartesiaTTSService
     from pipecat.services.openai import OpenAILLMService
     from pipecat.transports.services.daily import DailyParams, DailyTransport
+    from loguru import logger
+
+    class SealionLLMService(OpenAILLMService):
+        """A service for interacting with Groq's API using the OpenAI-compatible interface.
+
+        This service extends OpenAILLMService to connect to Groq's API endpoint while
+        maintaining full compatibility with OpenAI's interface and functionality.
+
+        Args:
+            api_key (str): The API key for accessing Groq's API
+            base_url (str, optional): The base URL for Groq API. Defaults to "https://api.groq.com/openai/v1"
+            model (str, optional): The model identifier to use. Defaults to "llama-3.1-70b-versatile"
+            **kwargs: Additional keyword arguments passed to OpenAILLMService
+        """
+
+        def __init__(
+            self,
+            *,
+            api_key: str,
+            base_url: str = "https://api.sea-lion.ai/v1",
+            model: str = "aisingapore/llama3-8b-cpt-sea-lionv2.1-instruct",
+            **kwargs,
+        ):
+            super().__init__(api_key=api_key, base_url=base_url, model=model, **kwargs)
+
+        def create_client(self, api_key=None, base_url=None, **kwargs):
+            """Create OpenAI-compatible client for SEALION API endpoint."""
+            logger.debug(f"Creating SEALION client with api {base_url}")
+            return super().create_client(api_key, base_url, **kwargs)
 
     transport = DailyTransport(
         room_url,
@@ -38,7 +67,10 @@ async def main(room_url: str, token: str):
         api_key=os.getenv("CARTESIA_API_KEY", ""), voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22"
     )
 
-    llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
+    # llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
+    llm = SealionLLMService(
+        api_key=os.getenv("AISG_API_KEY"),
+    )
 
     messages = [
         {
